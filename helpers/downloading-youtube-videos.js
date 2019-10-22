7f4785d733651b3e4e6a1d610215a44b9d85fded
Eduardo Gonzalez <edlu.gora96@gmail.com>
2019-10-21 23:12:52 -0400

    Delete video/channel Fixed

--- helpers/downloading-youtube-videos.js
+++ helpers/downloading-youtube-videos.js
@@ -109,7 +109,7 @@ YoutubeVideos.prototype.processDownload = function(doc) {
                                     .then(result => {
                                         //console.log(res);
                                     });
-                                // this.uploadToAWSS3(output, doc._id);
+                                this.uploadToAWSS3(output, doc._id);
                                 this.isLoading = false;
                             }
                         }
