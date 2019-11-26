6e69e02b6b09cf8ec59bfedcbd69dc4f63092c58
Joshua Saucedo <joshuansu0897@gmail.com>
2019-11-26 11:12:35 -0700

    update date moment

--- client/containers/admin/MasterVideoList/MasterVideoListView.js
+++ client/containers/admin/MasterVideoList/MasterVideoListView.js
@@ -51,7 +51,6 @@ const MasterVideoListView = props => {
           >
             {videoList &&
               videoList.map((video, index) => {
-                console.log(video)
                 return (
                   <div style={{ flexDirection: "column" }} key={index}>
                     <div className="videoCard">
@@ -121,7 +120,7 @@ const MasterVideoListView = props => {
                       <div className="videoCardTitle">{video.title}</div>
                     </div>
                     <div className="videoCardFooterTitle">{video.channelName}</div>
-                    <div className="videoCardFooterTitle">{Moment(video.create).format('d MMM')}</div>
+                    <div className="videoCardFooterTitle">{Moment(video.create).format('d MMM YYYY')}</div>
                     <div
                       style={{
                         margin: "10px"
