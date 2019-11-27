67184fa64d9413a48a969419cd92c7714a5e839e
Joshua Saucedo <joshuansu0897@gmail.com>
2019-11-27 11:09:30 -0700

    add return

--- models/users.js
+++ models/users.js
@@ -626,10 +626,12 @@ UserModel.prototype.removeMany = function (ids) {
               this.modelDB.findById(id, (err, doc) => {
                 if (doc === null || doc === undefined) {
                   callback(null);
+                  return
                 }
 
                 if (doc.avatar === null || doc.avatar === undefined) {
                   callback(null);
+                  return
                 }
 
                 var _path = doc.avatar.replace("/static", "../public");
